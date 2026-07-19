2e9a054e29fd8cc5d66fb972caa83c87275d424b
Luca <panda.blau@gmx.de>
2026-07-19 21:09:48 +0200

    added csv headers for convert pixel

--- src/converter.rs
+++ src/converter.rs
@@ -1,7 +1,7 @@
-use std::{fs, io::{self, BufWriter, Write}, path,
-    time::{
-        Instant,
-    },
+use std::{
+    fs,
+    io::{self, BufWriter, Write}, 
+    path,
 };
 use log::{warn};
 use indicatif::{ProgressBar, ProgressStyle};
@@ -13,7 +13,6 @@ use rayon::prelude::*;
 
 use crate::{
     hostrada_variable::HostradaVar,
-    error::HostradaError,
     hostrada_dataset::HostradaDataset,
     misc::green_spinner,
 };
@@ -21,10 +20,13 @@ use crate::{
 /// Converts all values of a file into csv format, in order time, y, x (this means that first, all x values for the first timestamp for the first y are displayed and so on).
 /// The use of this function is strongly discouraged, as >700 files are created (1 for each hour).
 /// Currently, x and y for each value are hard coded (x 0-719, y 0-937)
-pub fn convert_all_values(files: Vec<path::PathBuf>, output_dir: path::PathBuf) -> Result<(), HostradaError> {
+pub fn convert_all_values(files: Vec<path::PathBuf>, output_dir: path::PathBuf) -> anyhow::Result<()> {
     warn!("The use of this functionality is strongly discouraged! Expect >700 csv files!");
     let spinner = green_spinner();
-    let (datasets, _, var_id) = setup(files, None, None)?;
+    let datasets = HostradaDataset::from_filelist_same_grids(files)?;
+    
+    // use first dataset to extract variable_id for output filename
+    let var_id = datasets.first().unwrap().var_id().ok_or(anyhow::anyhow!("Did not found a var id for merged file"))?;
 
     let mut dataset_count = 1;
 
@@ -39,21 +41,10 @@ pub fn convert_all_values(files: Vec<path::PathBuf>, output_dir: path::PathBuf) 
         let end_date = dataset.end_date().unwrap().0.clone();
 
         // progressbar stuff
-        let diff = (end_date-current).num_hours();
-        
-        let pb = ProgressBar::new(diff as u64);
-
-        pb.set_style(ProgressStyle::with_template(
-            "{msg} {spinner:.green} [{bar:40.green}] {percent}% [{eta}]"
-        ).unwrap().progress_chars("->o"));
-
+        let diff = (end_date-current).num_hours() as u64;
+        let pb = converter_pb(diff);
         pb.set_message(format!("Converting dataset {}/{}", dataset_count, datasets.len()));
 
-        pb.enable_steady_tick(std::time::Duration::from_millis(100));
-
-
-
-        let start = Instant::now();
         // actual writing and increment progressbar
         while current <= end_date {
             // setup hourly file
@@ -66,7 +57,6 @@ pub fn convert_all_values(files: Vec<path::PathBuf>, output_dir: path::PathBuf) 
             writeln!(wtr, "{},pixel_x, pixel_y", var_id)?;
 
 
-            // let inner_start = Instant::now();
 
             let hourly_data  = dataset.file().variable(&var_id).unwrap().get::<f32, _>((dataset.time_index(&current).unwrap(), .., ..))?;
 
@@ -90,7 +80,6 @@ pub fn convert_all_values(files: Vec<path::PathBuf>, output_dir: path::PathBuf) 
 
                 writeln!(wtr, "{:.2},{},{}", val, &x_y[idx].0, &x_y[idx].1).unwrap();
             }
-            // println!("One date iteration: {:?}", inner_start.elapsed());
 
 
             current += Duration::hours(1);
@@ -98,54 +87,12 @@ pub fn convert_all_values(files: Vec<path::PathBuf>, output_dir: path::PathBuf) 
 
         }
 
-        println!("Time for one dataset: {:?}", start.elapsed());
-        pb.finish();
+        pb.finish_with_message(format!("Done with 1 dataset. ({:.02}s)", pb.elapsed().as_secs_f32()));
         dataset_count += 1;
 
     }
 
     Ok(())
-}
-
-
-// ISSUES:
-    // Unterschiedliche Dateien für unterschiedliche Variablen
-    // nicht zusammenhängende downloads (z.B. 2001 und 2005 aber nicht 2002-2004)
-// LÖSUNG:
-    // Alles einzelne dateien, bei einem --merge befehl werden sie aber dann zusammen in eine datei geschrieben 
-        // ist die frage ob der nutzer dann einfach drauf achten soll, dass in dem directory nur eine variable mit konsistenter zeit abgebildet ist... mal schauen
-
-
-fn setup(files: Vec<path::PathBuf>, x_y: Option<(usize, usize)>, output_dir: Option<&mut path::PathBuf>) -> Result<(Vec<HostradaDataset>, Option<io::BufWriter<fs::File>>, String), HostradaError> {
-    // can also be 1 dataset in single file mode
-    let datasets = HostradaDataset::from_filelist_same_grids(files)?;
-    
-    // use first dataset to extract variable_id for output filename
-    let first = datasets.first().unwrap();
-    let var_id = first.var_id().unwrap_or_else(|| {
-        eprintln!("File {:?} does not seem to have any variable id. Is this a hostrada file? If everything should work, this is a bug. If this is indeed not a hostrada hourly netcdf file, this is expected behavior.", first.file().path());
-        std::process::exit(1);
-    });
-
-
-    if let Some(dir) = output_dir {
-
-        // use last dataset for file name
-        let last = datasets.last().unwrap();
-        let output_filename = if let Some(coords) = x_y {
-            path::PathBuf::from(&format!("{}_Pix_{}_{}_{:?}-{:?}_converted.csv", var_id, coords.0, coords.1, first.start_date().unwrap().0, last.end_date().unwrap().0))
-        } else {
-            return Err(std::io::Error::other("Internal Error: did not pass x_y while passing output_dir.").into());
-        };
-        dir.push(output_filename);
-        let file = fs::File::create(dir)?;
-        let wtr = io::BufWriter::new(file);
-        return Ok((datasets, Some(wtr), var_id))
-    };
-
-    Ok((datasets, None, var_id))
-
-
 }
 
 
@@ -159,7 +106,10 @@ pub fn convert_pixel(files: Vec<path::PathBuf>, x: usize, y: usize, output_dir: 
             .join(format!("merged_{}_pix_{}_{}_{}_merged.csv",var_id, x, y, Local::now().to_rfc3339()));
         let file = fs::File::create(file_name)?;
 
-        ConvertMode::Combined(io::BufWriter::new(file))
+        let mut wtr = io::BufWriter::new(file);
+        write!(wtr, "timestamp, {}", var_id)?;
+
+        ConvertMode::Combined(wtr)
 
     } else {
         ConvertMode::Seperate()
@@ -179,6 +129,9 @@ fn convert_dataset(dataset: HostradaDataset, x: usize, y: usize, output_dir: &pa
 
     let path = dataset.file().path()?;
 
+    let var_id = dataset.var_id()
+    .ok_or(anyhow::anyhow!("Failed to get variable id for {}", path.display()))?;
+
     // illegal stuff to make this work without code duplication
     let mut local_wtr;
     let wtr = match mode {
@@ -189,14 +142,14 @@ fn convert_dataset(dataset: HostradaDataset, x: usize, y: usize, output_dir: &pa
             let file = fs::File::create(res_file_name)?;
             local_wtr = io::BufWriter::new(file);
 
+            write!(local_wtr, "timestamp, {}", var_id)?;
+
+
             &mut local_wtr
         },
         ConvertMode::Combined(wtr) => wtr,
     };
     
-    let var_id = dataset.var_id()
-        .ok_or(anyhow::anyhow!("Failed to get variable id for {}", path.display()))?;
-
     let pb = converter_pb(diff);
     pb.set_message(format!("Converting pixel ({}/{}) {:?}...", x, y, path));
 
